d1251d8792f36dfc82bc365d9f0a18f17b1cae77
sheshu22 <reddysheshasena@gmail.com>
2022-03-21 00:39:43 +0530

    added lowercase function

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -2,11 +2,17 @@ import React,{useState} from 'react'
 
 export default function Textform(props) {
 
-  const[text,setText]=useState("first hook");
+  const[text,setText]=useState("Enter Text");
 
   const upClick= ()=>{
     console.log("uppercase button clicked");
     let updatedText= text.toUpperCase();
+    setText(updatedText);
+  }
+
+  const lowclick=()=>{
+    console.log("lowercase button clicked");
+    let updatedText= text.toLowerCase();
     setText(updatedText);
   }
 
@@ -20,10 +26,12 @@ export default function Textform(props) {
     <>
     <div className="container mb-3 my-3 ">
         <h2 className='title1' >{props.title}</h2>
-        <h4>ENTER THE TEXT</h4>
+        <h4>ENTER THE TEXT BELOW</h4>
         <textarea className="form-control " value= {text} onChange={change} id="exampleFormControlTextarea1" rows="9"></textarea>
 
-        <button type="button" className="btn btn-primary btttt" onClick={upClick} >Uppercase</button>
+        <button type="button" className="btn btn-primary mx-2 btttt" onClick={upClick} >Uppercase</button>
+        <button type="button" className="btn btn-primary mx-2 btttt" onClick={lowclick} >Lowercase</button>
+
     </div>
 
     <div className="container my-3">
