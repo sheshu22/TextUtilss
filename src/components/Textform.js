5411efde3d386b0da6aff18c9e94b79c8803eb15
sheshu22 <reddysheshasena@gmail.com>
2022-03-21 00:28:20 +0530

    added summary and preview

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -16,15 +16,25 @@ export default function Textform(props) {
     setText(event.target.value);
   }
 
-  //setText("second hook");
   return (
+    <>
     <div className="container mb-3 my-3 ">
         <h2 className='title1' >{props.title}</h2>
         <h4>ENTER THE TEXT</h4>
         <textarea className="form-control " value= {text} onChange={change} id="exampleFormControlTextarea1" rows="9"></textarea>
 
         <button type="button" className="btn btn-primary btttt" onClick={upClick} >Uppercase</button>
+    </div>
+
+    <div className="container my-3">
+
+      <h3>TEXT SUMMARY</h3>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.03 * text.split(" ").length} Minutes read</p>
+      <h3> PREVIEW </h3>
+      <p>{text}</p>
 
     </div>
+    </>
   )
 }
