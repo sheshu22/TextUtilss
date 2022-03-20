8be4d03810eaaef2ea3b254913fff4965349e4bc
sheshu22 <reddysheshasena@gmail.com>
2022-03-20 23:52:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+import Textform from './components/Textform';
+function App() {
+  return (
+    <>
+     <Header title= "TextUtils" one="Home" two="About"/> 
+     <Textform title="TEXTUTILS"/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Textform.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+
+export default function Textform(props) {
+
+  const[text,setText]=useState("first hook");
+
+  const upClick= ()=>{
+    let updatedText= text.toUpperCase();
+    setText(updatedText);
+  }
+
+  const change=(event)=>{
+    setText(event.target.value);
+  }
+
+  //setText("second hook");
+  return (
+    <div className="container mb-3 my-3 ">
+        <h2 className='title1' >{props.title}</h2>
+        <h4>ENTER THE TEXT</h4>
+        <textarea className="form-control " value= {text} onChange={change} id="exampleFormControlTextarea1" rows="9"></textarea>
+
+        <button type="button" className="btn btn-primary btttt" onClick={upClick} >Uppercase</button>
+
+    </div>
+  )
+}
